e07c01ca379a197205cc948cfa2f19eea7d366b7
Lesleyvm <lvanmusscher@gmail.com>
2023-09-13 21:11:22 +0200

    finished excercise

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [light, toggleLight] = React.useState('false');
+    // const toggleLightSwitch = () => {
+    //     toggleLight(!light);
+    // };
+    // kan eventueel direct in de clickHandler worden aangeroepen, wellicht overzichtelijker. Er is dan geen anonieme functie nodig (staat al in const) en de switch kan direct binnen {} worden aangeroepen.
+
+    console.log(light)
+
     return (
-        <main className="off">
+        <main className={light === true ? 'on' : 'off'}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button"
+                        onClick={() => toggleLight(!light)}>{light ? 'Turn off' : 'Turn on'}</button>
             </section>
         </main>
     );
